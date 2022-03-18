28919a4f56236b87726ea9754a4048f0bfef2e31
Loki <uddeeptaraajkashyap@gmail.com>
2022-03-18 17:03:05 +0000

    updated mac_changer.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,8 +1,15 @@
+from tabnanny import verbose
 import scapy.all as scapy
+from sympy import false
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-    scapy.ls(scapy.ARP)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-scan("172.31.150.1/24")
+    print("IP\t\t\tMAC Address\n------------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc +"\t\t"+ element[1].hwsrc);
+
+scan("172.31.150.1/21")
