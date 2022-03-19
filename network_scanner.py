3355fd28b50ca84db2de872a4324e72c1b15175b
Uddeepta Raaj Kashyap <uddeeptaraajkashyap@gmail.com>
2022-03-19 02:49:46 +0000

    optparser added

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,20 @@
-from tabnanny import verbose
+
 import scapy.all as scapy
-from sympy import false
+import optparse
+
+
+def get_argument():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-t","--target", dest = "ip", help="IP address or range of IP Address to scan")
+
+    (options, arguments)=parser.parse_args()
+
+    if not options.ip:
+        parser.error("[-] Please specify an ip, use --help for more info.")
+    return options
+
+
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -8,8 +22,25 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\tMAC Address\n------------------------------------------")
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc +"\t\t"+ element[1].hwsrc);
+        clients_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(clients_dict)
+    return clients_list
 
-scan("172.31.150.1/21")
+
+def print_result(results_list):
+
+    print("IP\t\t\tMAC Address\n------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+options= get_argument() 
+
+scan_result = scan(options.ip)
+print_result(scan_result)
+
+
+
+
